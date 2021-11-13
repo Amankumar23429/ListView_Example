7b34e6373ac8deec1141b544925b664aebb71cba
Amankumar23429 <amankumar64865@gmail.com>
2021-11-13 13:02:51 +0530

    save favourite functionality

--- app/src/main/java/com/example/listviewexample/Custom_ListView.kt
+++ app/src/main/java/com/example/listviewexample/Custom_ListView.kt
@@ -1,15 +1,18 @@
 package com.example.listviewexample
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ListView
 
 
 class Custom_ListView : AppCompatActivity() {
+    val FAVOURITE_KEY = "FAVOURITE"
     val cityData = fillCitydata()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_custom_list_view)
+        loadFavourite()
 
         val cities = findViewById<ListView>(R.id.list_view2)
 
@@ -22,9 +25,29 @@ class Custom_ListView : AppCompatActivity() {
                 city.favourite = !city.favourite
                 cityAdapter.notifyDataSetChanged()
             }
+            saveFavourites()
 
         }
 
+    }
+
+    private fun saveFavourites(){
+        val sharedPref = getPreferences(Context.MODE_PRIVATE)
+        val favourites = cityData.filter { it.favourite }.map { it.country }
+        with(sharedPref.edit()){
+            putStringSet(FAVOURITE_KEY, favourites.toSet())
+            commit()
+        }
+    }
+
+    private fun loadFavourite(){
+        val sharedPref = getPreferences(Context.MODE_PRIVATE)
+        val favourites = sharedPref.getStringSet(FAVOURITE_KEY, null)
+
+        favourites?.forEach{country->
+            val city = cityData.find{it.country==country}
+            city?.favourite=true
+        }
     }
 
     private fun fillCitydata():Array<City>{
